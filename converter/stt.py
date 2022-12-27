7fa0d9b201f0848488ada8d2839ec9c418e53912
chawanghyeon <chawanghyeon@gmail.com>
2022-12-28 00:03:30 +0900

    test stt with text classification

--- converter/stt.py
+++ converter/stt.py
@@ -22,3 +22,19 @@ model = whisper.load_model("large")
 
 result = model.transcribe("test.mp3")
 print(result["text"])
+
+from transformers import ElectraTokenizer, ElectraForSequenceClassification, pipeline
+
+model_name = "monologg/koelectra-base-finetuned-nsmc"
+tokenizer = ElectraTokenizer.from_pretrained(model_name)
+model = ElectraForSequenceClassification.from_pretrained(model_name)
+
+nsmc = pipeline(
+    "sentiment-analysis",
+    tokenizer=tokenizer,
+    model=model
+)
+
+print(nsmc("이 영화는 미쳤다. 넷플릭스가 일상화된 시대에 극장이 존재해야하는 이유를 증명해준다."))
+print(nsmc("와 진짜 너무 재미없네요는 뻥이고 진짜 너무 재밌어요"))
+print(nsmc(result["text"]))
