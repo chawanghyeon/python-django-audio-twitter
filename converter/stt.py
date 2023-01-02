5380164e495c947046ec99a32e601a87114a8beb
chawanghyeon <chawanghyeon@gmail.com>
2023-01-02 23:21:43 +0900

    modify stt

--- converter/stt.py
+++ converter/stt.py
@@ -1,17 +1,26 @@
 from transformers import ElectraTokenizer, ElectraForSequenceClassification, pipeline
 import whisper
+from collections import deque
 
 whisper_model = whisper.load_model("small", device="cpu")
-stt = whisper_model.transcribe("test.mp3", fp16=False)
 
 model_name = "monologg/koelectra-base-finetuned-nsmc"
 tokenizer = ElectraTokenizer.from_pretrained(model_name)
-model = ElectraForSequenceClassification.from_pretrained(model_name)
+electra_model = ElectraForSequenceClassification.from_pretrained(model_name)
 
 analyzer = pipeline(
     "sentiment-analysis",
     tokenizer=tokenizer,
-    model=model
+    model=electra_model
 )
 
-print(analyzer(stt.text))
+queuue = deque([])
+
+while True:
+    if len(queuue) == 0:
+        continue
+    task = queuue.popleft()
+    stt = whisper_model.transcribe(task)
+    analyzer(stt.text)
+
+
