3f48de14c338f90dc47dd3b3d9c1215ce1ce7871
chawanghyeon <chawanghyeon@gmail.com>
2023-01-15 01:46:21 +0900

    Add type hint in serializers.py

--- babble/serializers.py
+++ babble/serializers.py
@@ -1,58 +1,59 @@
-from rest_framework.serializers import (CharField, ModelSerializer,
-                                        StringRelatedField)
+from rest_framework.serializers import CharField, ModelSerializer, StringRelatedField
 
 from .models import *
 
 
 class CommentSerializer(ModelSerializer):
-    user = StringRelatedField(many=False)
+    user: StringRelatedField = StringRelatedField(many=False)
 
     class Meta:
-        model = Comment
-        depth = 1
-        exclude = ("babble",)
+        model: Comment = Comment
+        depth: int = 1
+        exclude: tuple = ("babble",)
 
 
 class FollowerSerializer(ModelSerializer):
-    user = StringRelatedField(many=False)
-    following = StringRelatedField(many=False)
+    user: StringRelatedField = StringRelatedField(many=False)
+    following: StringRelatedField = StringRelatedField(many=False)
 
     class Meta:
-        fields = "__all__"
-        model = Follower
-        depth = 1
+        fields: str = "__all__"
+        model: Follower = Follower
+        depth: int = 1
 
 
 class LikeSerializer(ModelSerializer):
-    user = StringRelatedField(many=False)
+    user: StringRelatedField = StringRelatedField(many=False)
 
     class Meta:
-        model = Like
-        exclude = ("babble",)
-        depth = 1
+        model: Like = Like
+        exclude: tuple = ("babble",)
+        depth: int = 1
 
 
 class TagSerializer(ModelSerializer):
     class Meta:
-        fields = "__all__"
-        model = Tag
-        depth = 1
+        fields: str = "__all__"
+        model: Tag = Tag
+        depth: int = 1
 
 
 class BabbleSerializer(ModelSerializer):
-    user = StringRelatedField(many=False)
+    user: StringRelatedField = StringRelatedField(many=False)
 
     class Meta:
-        fields = "__all__"
-        model = Babble
-        depth = 1
+        fields: str = "__all__"
+        model: Babble = Babble
+        depth: int = 1
 
 
 class UserSerializer(ModelSerializer):
-    babbles = BabbleSerializer(many=True, read_only=True, required=False)
-    password = CharField(write_only=True, required=False)
+    babbles: BabbleSerializer = BabbleSerializer(
+        many=True, read_only=True, required=False
+    )
+    password: CharField = CharField(write_only=True, required=False)
 
     class Meta:
-        fields = "__all__"
-        model = User
-        depth = 1
+        fields: str = "__all__"
+        model: User = User
+        depth: int = 1
