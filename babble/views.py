e40f75881dcef45dfb1f9499935838cd91640b68
chawanghyeon <chawanghyeon@gmail.com>
2023-01-31 12:43:41 +0900

    Remove library

--- babble/views.py
+++ babble/views.py
@@ -8,7 +8,6 @@ from django.db import DatabaseError, transaction
 from django.db.models import F, Q
 from django.db.models.manager import BaseManager
 from django.http import FileResponse, HttpRequest
-from django.utils import timezone
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
