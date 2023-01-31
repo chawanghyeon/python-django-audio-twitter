5707aceb380e22d455f007664eb9db0a3eea7cd6
chawanghyeon <chawanghyeon@gmail.com>
2023-01-31 12:56:51 +0900

    Edit utils.py

--- babble/utils.py
+++ babble/utils.py
@@ -8,5 +8,5 @@ from django.utils import timezone
 def image_file_path(instance: Any, filename: str) -> str:
     now: datetime = timezone.now()
     filepath: Any = now.strftime("image/%Y/%m/%d")
-    filename: str = str(instance.id) + str(now.strftime("%H%M%S")) + ".jpg"
+    filename: str = str(instance.id) + "-" + str(now.strftime("%H%M%S")) + ".jpg"
     return os.path.join(filepath, filename)
