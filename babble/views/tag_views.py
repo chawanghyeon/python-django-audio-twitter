c116b43d95cbd859cb6d2814e3c689c412bf0704
chawanghyeon <chawanghyeon@gmail.com>
2023-02-24 23:09:43 +0900

    Refactor tag_views.py

--- babble/views/tag_views.py
+++ babble/views/tag_views.py
@@ -11,23 +11,16 @@ from ..serializers import *
 
 
 class TagViewSet(viewsets.ModelViewSet):
-
-    queryset: BaseManager[Tag] = Tag.objects.all()
-    serializer_class: Type[TagSerializer] = TagSerializer
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
 
     @action(detail=False, methods=["get"], url_path="<str:tag>")
     def get_babbles_with_tag(
         self, request: HttpRequest, tag: Optional[str] = None
     ) -> Response:
+        tags = Tag.objects.filter(name=tag)
+        babbles = Babble.objects.filter(tags__in=tags)
 
-        tags: List[Tag] = self.queryset.filter(text=tag)
-
-        if tags is None:
-            return Response(
-                {"error": "Tags not found"}, status=status.HTTP_404_NOT_FOUND
-            )
-
-        babbles: List[Babble] = Babble.objects.filter(tags=tags)
-        serializer: BabbleSerializer = BabbleSerializer(babbles, many=True)
+        serializer = BabbleSerializer(babbles, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
