c49f38cd69186e3509e2a0ce825b919f250a105c
chawanghyeon <chawanghyeon@gmail.com>
2023-02-24 23:05:57 +0900

    Refactor like_views.py

--- babble/views/like_views.py
+++ babble/views/like_views.py
@@ -12,72 +12,40 @@ from ..serializers import *
 
 
 class LikeViewSet(viewsets.ModelViewSet):
-    queryset: BaseManager[Like] = Like.objects.all()
-    serializer_class: Type[LikeSerializer] = LikeSerializer
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
 
+    @transaction.atomic
     def create(self, request: HttpRequest) -> Response:
-        serializer: LikeSerializer = LikeSerializer(data=request.data)
+        serializer = LikeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        if serializer.is_valid() == False:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        try:
-            with transaction.atomic():
-                serializer.save()
-
-                babble: Optional[Babble] = Babble.objects.get_or_none(
-                    pk=request.data.get("babble")
-                )
-
-                if babble is None:
-                    raise DatabaseError
-
-                babble.update(like_count=F("like_count") + 1)
-
-        except DatabaseError:
-            return Response({"error": "Cancle like"}, status=status.HTTP_409_CONFLICT)
+        babble = Babble.objects.get_or_404(id=request.data.get("babble"))
+        babble.update(like_count=F("like_count") + 1)
 
         return Response(
             {"message": "Like created successfully"}, status=status.HTTP_201_CREATED
         )
 
-    def destroy(self, request: HttpRequest, pk: Optional[int] = None) -> Response:
-        like: Optional[Like] = Like.objects.get_or_none(pk=pk)
+    @transaction.atomic
+    def destroy(self, request: HttpRequest, id: Optional[int] = None) -> Response:
+        like = Like.objects.select_related("babble").get_or_404(id=id)
 
-        if like is None:
-            return Response(
-                {"error": "Like not found"}, status=status.HTTP_404_NOT_FOUND
-            )
-
-        try:
-            with transaction.atomic():
-                babble: Optional[Babble] = Babble.objects.get_or_none(pk=like.babble.id)
-
-                if babble is None:
-                    raise DatabaseError
-
-                babble.update(like_count=F("like_count") - 1)
-                like.delete()
-
-        except DatabaseError:
-            return Response({"error": "Cancle unlike"}, status=status.HTTP_409_CONFLICT)
+        like.babble.update(like_count=F("like_count") - 1)
+        like.delete()
 
         return Response(
             {"message": "Like deleted successfully"}, status=status.HTTP_200_OK
         )
 
-    def list(self, request: HttpRequest, pk: Optional[int] = None) -> Response:
-        if pk is None:
-            user: User = request.user
+    def list(self, request: HttpRequest, id: Optional[int] = None) -> Response:
+        if id:
+            user = User.objects.get_or_404(id=id)
         else:
-            user: Optional[User] = User.objects.get_or_none(pk=pk)
+            user = request.user
 
-        if user is None:
-            return Response(
-                {"error": "User not found"}, status=status.HTTP_404_NOT_FOUND
-            )
-
-        likes: BaseManager[Like] = self.queryset.filter(user=user)
-        serializer: LikeBabbleSerializer = LikeBabbleSerializer(likes, many=True)
+        likes = self.queryset.filter(user=user)
+        serializer = LikeBabbleSerializer(likes, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
