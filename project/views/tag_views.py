05dc1c311fe7e960e4f31dd5d4f0a1e10a32fd38
chawanghyeon <chawanghyeon@gmail.com>
2023-03-06 14:21:18 +0900

    Done Tag logic

--- project/views/tag_views.py
+++ project/views/tag_views.py
@@ -2,24 +2,25 @@ from typing import Optional
 
 from django.http import HttpRequest
 from rest_framework import status, viewsets
-from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from project.models import Babble, Tag
 from project.serializers import BabbleSerializer, TagSerializer
+from project.views.views_utils import check_liked, check_rebabbled
 
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
-    @action(detail=False, methods=["get"], url_path="<str:tag>")
-    def get_babbles_with_tag(
-        self, request: HttpRequest, tag: Optional[str] = None
-    ) -> Response:
-        tags = Tag.objects.filter(name=tag)
-        babbles = Babble.objects.filter(tags__in=tags)
+    def retrieve(self, request: HttpRequest, pk: Optional[str] = None) -> Response:
+        tag = Tag.objects.filter(text=pk)
+        babbles = Babble.objects.filter(tags__in=tag).order_by("-created")
 
         serializer = BabbleSerializer(babbles, many=True)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        serialized_data = serializer.data
+        serialized_data = check_rebabbled(serialized_data, request.user)
+        serialized_data = check_liked(serialized_data, request.user)
+
+        return Response(serialized_data, status=status.HTTP_200_OK)
