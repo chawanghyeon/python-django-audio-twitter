49e7a0dd4eeda7455255ca15e965752888af8ead
chawanghyeon <chawanghyeon@gmail.com>
2023-03-11 13:18:13 +0900

    Test logging

--- project/views/babble_views.py
+++ project/views/babble_views.py
@@ -38,40 +38,59 @@ class BabbleViewSet(viewsets.ModelViewSet):
 
     @transaction.atomic
     def create(self, request: HttpRequest) -> Response:
-        logger.info(
-            f"Received POST request to create a new babble by user {request.user.username}"
-        )
+        log_data = {
+            "user": request.user.username,
+            "model": "Babble",
+            "action": "create",
+            "status": "",
+            "method": request.method,
+        }
+
+        log_data["status"] = status.HTTP_102_PROCESSING
+        logger.info(log_data)
 
         serializer = BabbleSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=False):
-            babble = serializer.save(user=request.user)
-            logger.info(
-                f"New babble created with id {babble.id} by user {request.user.username}"
-            )
+        if not serializer.is_valid(raise_exception=False):
+            log_data["status"] = status.HTTP_400_BAD_REQUEST
+            logger.error(log_data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-            babble = save_keywords(babble)
-            serializer = BabbleSerializer(babble)
-            logger.info(
-                f"Keywords are {serializer.data.get('tags')} for babble with id {babble.id} by user {request.user.username}"
-            )
-            set_caches(babble, request.user, serializer.data)
+        babble = serializer.save(user=request.user)
+        babble = save_keywords(babble)
+        serializer = BabbleSerializer(babble)
+        set_caches(babble, request.user, serializer.data)
 
-            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+        log_data["status"] = status.HTTP_201_CREATED
+        log_data["babble_id"] = babble.id
+        log_data["keywords"] = serializer.data["keywords"]
+        logger.info(log_data)
 
-        else:
-            response = Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            logger.warning(
-                f"Validation errors occurred while creating a new babble by user {request.user.username}: {serializer.errors}"
-            )
-
-        return response
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request: HttpRequest, pk: Optional[str] = None) -> Response:
+        log_data = {
+            "user": request.user.username,
+            "model": "Babble",
+            "action": "retrieve",
+            "status": "",
+            "babble_id": pk,
+            "method": request.method,
+        }
+
+        log_data["status"] = status.HTTP_102_PROCESSING
+        logger.info(log_data)
+
         pk = int(pk)
         babble_data = babble_cache.get(pk)
 
         if babble_data is None:
-            babble = Babble.objects.get(id=pk)
+            try:
+                babble = Babble.objects.get(id=pk)
+            except Babble.DoesNotExist:
+                log_data["status"] = status.HTTP_404_NOT_FOUND
+                logger.error(log_data)
+                return Response(status=status.HTTP_404_NOT_FOUND)
+
             babble_data = BabbleSerializer(babble).data
             babble_cache.set(pk, babble_data)
 
@@ -81,30 +100,84 @@ class BabbleViewSet(viewsets.ModelViewSet):
         babble_data["is_rebabbled"] = is_rebabbled
         babble_data["is_liked"] = is_liked
 
+        log_data["status"] = status.HTTP_200_OK
+        logger.info(log_data)
+
         return Response(babble_data, status=status.HTTP_200_OK)
 
     @transaction.atomic
     def update(self, request: HttpRequest, pk: Optional[str] = None) -> Response:
-        babble = Babble.objects.get_or_404(id=pk)
+        log_data = {
+            "user": request.user.username,
+            "model": "Babble",
+            "action": "update",
+            "status": "",
+            "babble_id": pk,
+            "method": request.method,
+        }
+        log_data["status"] = status.HTTP_102_PROCESSING
+        logger.info(log_data)
+
+        try:
+            babble = Babble.objects.get(id=pk)
+        except Babble.DoesNotExist:
+            log_data["status"] = status.HTTP_404_NOT_FOUND
+            logger.error(log_data)
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
         serializer = BabbleSerializer(babble, data=request.data)
         serializer.is_valid(raise_exception=True)
 
         babble = serializer.save()
         babble = save_keywords(babble)
-
         set_follower_cache(babble, request.user)
 
-        return Response(BabbleSerializer(babble).data, status=status.HTTP_200_OK)
+        serializer = BabbleSerializer(babble)
+        log_data["status"] = status.HTTP_200_OK
+        log_data["keywords"] = serializer.data["keywords"]
+        logger.info(log_data)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @transaction.atomic
     def destroy(self, request: HttpRequest, pk: Optional[str] = None) -> Response:
+        log_data = {
+            "user": request.user.username,
+            "model": "Babble",
+            "action": "destroy",
+            "status": "",
+            "babble_id": pk,
+            "method": request.method,
+        }
+
+        log_data["status"] = status.HTTP_102_PROCESSING
+        logger.info(log_data)
         pk = int(pk)
-        Babble.objects.filter(id=pk).delete()
-        babble_cache.delete(pk)
+
+        try:
+            Babble.objects.filter(id=pk).delete()
+        except Exception:
+            log_data["status"] = status.HTTP_404_NOT_FOUND
+            logger.error(log_data)
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        log_data["status"] = status.HTTP_200_OK
+        logger.info(log_data)
 
         return Response(status=status.HTTP_200_OK)
 
     def list(self, request: HttpRequest) -> Response:
+        log_data = {
+            "user": request.user.username,
+            "model": "Babble",
+            "action": "list",
+            "status": "",
+            "method": request.method,
+        }
+
+        log_data["status"] = status.HTTP_102_PROCESSING
+        logger.info(log_data)
+
         user = get_user(request)
         user_cache_data = user_cache.get(user.id)
         next = request.query_params.get("next") or 0
@@ -112,8 +185,14 @@ class BabbleViewSet(viewsets.ModelViewSet):
 
         if user_cache_data:
             serialized_data = get_babbles_from_cache(user_cache_data, user, next)
+            log_data["status"] = status.HTTP_200_OK
+            log_data["from"] = "cache"
+            logger.info(log_data)
         else:
             serialized_data = get_babbles_from_db(user, next)
+            log_data["status"] = status.HTTP_200_OK
+            log_data["from"] = "db"
+            logger.info(log_data)
 
         if user != request.user:
             serialized_data = check_rebabbled(serialized_data, request.user)
@@ -129,21 +208,49 @@ class BabbleViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=["get"])
     def explore(self, request: HttpRequest) -> Response:
+        log_data = {
+            "user": request.user.username,
+            "model": "Babble",
+            "action": "explore",
+            "status": "",
+            "method": request.method,
+        }
+
+        log_data["status"] = status.HTTP_102_PROCESSING
+        logger.info(log_data)
+
         pagenator = CursorPagination()
         babbles = Babble.objects.exclude(user=request.user)
         babbles = pagenator.paginate_queryset(babbles, request)
 
         if babbles is None:
+            log_data["status"] = status.HTTP_404_NOT_FOUND
+            logger.error(log_data)
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         serializer = BabbleSerializer(babbles, many=True)
         serialized_data = check_rebabbled(serializer.data, request.user)
         serialized_data = check_liked(serialized_data, request.user)
 
+        log_data["status"] = status.HTTP_200_OK
+        logger.info(log_data)
+
         return pagenator.get_paginated_response(serialized_data)
 
     @action(detail=True, methods=["get"])
     def profile(self, request: HttpRequest, pk: Optional[str] = None) -> Response:
+        log_data = {
+            "user": request.user.username,
+            "model": "Babble",
+            "action": "profile",
+            "status": "",
+            "babble_id": pk,
+            "method": request.method,
+        }
+
+        log_data["status"] = status.HTTP_102_PROCESSING
+        logger.info(log_data)
+
         user = get_user(request, pk)
         pagenator = CursorPagination()
 
@@ -151,10 +258,15 @@ class BabbleViewSet(viewsets.ModelViewSet):
         babbles = pagenator.paginate_queryset(babbles, request)
 
         if babbles is None:
+            log_data["status"] = status.HTTP_404_NOT_FOUND
+            logger.error(log_data)
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         serializer = BabbleSerializer(babbles, many=True)
         serialized_data = check_rebabbled(serializer.data, request.user)
         serialized_data = check_liked(serialized_data, request.user)
+
+        log_data["status"] = status.HTTP_200_OK
+        logger.info(log_data)
 
         return pagenator.get_paginated_response(serialized_data)
